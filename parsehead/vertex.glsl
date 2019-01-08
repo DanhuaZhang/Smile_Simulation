6fecc279ac725691ac39dab6ce8e6e6b478fbbb8
DanHuaZhang <zhangdanhua3110@gmail.com>
2019-01-07 18:01:58 -0600

    Fixing merge conflicts with face_points and vertex.glsl

--- parsehead/vertex.glsl
+++ parsehead/vertex.glsl
@@ -18,9 +18,10 @@ uniform mat4 proj;
 
 void main() {
    Color = inColor;
-   gl_Position = proj * view * model * vec4(position,1.0);
+   gl_Position = proj * view * model * vec4(position,1.0)
    pos = (view * model * vec4(position,1.0)).xyz;
    lightDir = (view * vec4(inLightDir,0.0)).xyz; //It's a vector!
    vec4 norm4 = transpose(inverse(view*model)) * vec4(inNormal,0.0);
    normal = normalize(norm4.xyz);
-}
+}  
+
